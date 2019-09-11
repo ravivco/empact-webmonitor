d06992fe7522247166d2e28a5c23ec94e46850a9
Dotunj <dotunjolaosho@yahoo.com>
2019-09-11 10:55:48 +0100

    fixes

--- src/Drivers/BaseMonitor.php
+++ src/Drivers/BaseMonitor.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\App;
 
 class BaseMonitor
 {
-    public $twitterMonitor;
-
     public const TWITTER = 'twitter';
     public const GOOGLE = 'google';
     public const VIGO = 'vigo';
