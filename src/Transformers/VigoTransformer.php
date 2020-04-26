5c8d3f43d64d57d14637a3c505e5c4b0fd80769e
ravivco <raviv@go-public.co.il>
2020-04-26 13:48:01 +0300

    Update vigo query text using keyword + highlights

--- src/Transformers/VigoTransformer.php
+++ src/Transformers/VigoTransformer.php
@@ -30,7 +30,7 @@ class VigoTransformer implements TransformerInterface
                 'title' => isset($item['title']) ? $item['title'] : null,
                 'body' => isset($item['text']) ? $item['text'] : null,
                 'date' => isset($item['date']) ? $item['date'] : null,
-                'query_text' => isset($item['keyword']) ? $item['keyword'] : null,
+                'query_text' => (isset($item['keyword']) && isset($item['highlights'])) ? $item['keyword']." - ".$item['highlights'] : (isset($item['highlights']) ? $item['highlights'] : null),
                 'query_time' => '',
                 'api_brand_name' => isset($item['brand_name']) ? $item['brand_name'] : null,
                 'api_brand_id' => isset($item['brand_id']) ? $item['brand_id'] : null
